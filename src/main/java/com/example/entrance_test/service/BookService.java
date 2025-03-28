872f227d7accb934b98f06e06e865843cf598fed
NguyenTHanhAn853202 <nguyenthanhanqt@gmail.com>
2025-03-28 20:11:43 +0700

    fix book service

--- src/main/java/com/example/entrance_test/service/BookService.java
+++ src/main/java/com/example/entrance_test/service/BookService.java
@@ -1,6 +1,7 @@
 package com.example.entrance_test.service;
 
 import com.example.entrance_test.model.Book;
+import com.example.entrance_test.repository.AuthorRepository;
 import com.example.entrance_test.repository.BookRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,9 +14,11 @@ import java.util.Optional;
 @Service
 public class BookService {
     private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
 
-    public BookService(BookRepository bookRepository) {
+    public BookService(BookRepository bookRepository, AuthorRepository authorRepository) {
         this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
     }
 
     public Page<Book> getBooks(int page, int size) {
@@ -40,7 +43,7 @@ public class BookService {
         Book book = optionalBook.get();
 
         authorRepository.findById(book.getAuthor().getId()).ifPresent(author -> {
-            if (author.getBook_quantity() > 0) {
+            if (author.getBook_quantity() > 0) { 
                 author.setBook_quantity(author.getBook_quantity() - 1);
                 authorRepository.save(author);
             }
